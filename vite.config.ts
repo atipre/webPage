128df344b3d62c54a5c596ce71d7355510437d4b
atipre <tipreaditya@gmail.com>
2025-01-18 14:32:24 -0500

    Fixed vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/webPage-1/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
